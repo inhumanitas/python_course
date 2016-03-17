b4bd2d503dec2ba93310abe72c6f0994634fafc0
Данис Валиуллин <valiullin@cg.ru>
2016-03-17 10:17:57 +0300

    Added context manager solution

--- exercize/context_manager.py
+++ exercize/context_manager.py
@@ -9,3 +9,21 @@
     with MyOpen('wrong_file_path') as fh:
         fh.readlines()
 """
+import os
+
+
+class MyOpen(object):
+    def __init__(self, file_name, mode='r'):
+        self.file, self.mode = file_name, mode
+
+    def __enter__(self):
+        # create file if there is no one
+        if not os.path.isfile(self.file):
+            fh = open(self.file, 'w')
+            fh.close()
+
+        self.fh = open(self.file, self.mode)
+        return self.fh
+
+    def __exit__(self, type, value, traceback):
+        self.fh.close()
